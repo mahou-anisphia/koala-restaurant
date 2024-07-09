71aab1e7775ad8f5b8489130e9a430bb8a7a4606
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-07-10 01:34:44 +0700

    modified css

--- src/app.js
+++ src/app.js
@@ -89,7 +89,11 @@ app.use(
 app.use(
   "/api/",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+  swaggerUi.setup(swaggerSpec, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
 );
 
 module.exports = app;
